234aaed4c6f45061071781261629bda6bb50687c
Panoskontos <panos96el@gmail.com>
2022-11-12 19:59:47 +0200

    first commit

---
+++ Sol1.java
@@ -0,0 +1,26 @@
+package panos;
+import java.util.*;
+
+public class Sol1 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+
+        // Complete this line
+        int b = scan.nextInt();
+
+        // Complete this line
+        int c = scan.nextInt();
+        scan.close();
+
+
+        System.out.println(a);
+        // Complete this line
+        System.out.println(b);
+        System.out.println(c);
+
+
+        // Complete this line
+    }
+}
