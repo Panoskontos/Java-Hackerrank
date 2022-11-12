220c4d7e716f0257d44564015450f4e968ba44da
Panoskontos <panos96el@gmail.com>
2022-11-13 01:30:36 +0200

    first commit

---
+++ sol4.java
@@ -0,0 +1,28 @@
+package panos;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class sol4 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+
+
+        int i =1;
+        while(i<=10){
+            System.out.println(N+" x "+i+" = "+N*i);
+            i++;
+        }
+
+        bufferedReader.close();
+    }
+}
