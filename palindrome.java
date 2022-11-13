426a8251a24dc3457428d0e845f3e2dedd72e036
Panoskontos <panos96el@gmail.com>
2022-11-13 19:50:21 +0200

    first commit

---
+++ palindrome.java
@@ -0,0 +1,31 @@
+package panos;
+
+
+
+
+import java.io.*;
+        import java.util.*;
+
+public class palindrome {
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        String str = A, reverseStr = "";
+
+        int strLength = str.length();
+
+        for (int i = (strLength - 1); i >=0; --i) {
+            reverseStr = reverseStr + str.charAt(i);
+        }
+
+        if (str.toLowerCase().equals(reverseStr.toLowerCase())) {
+            System.out.println("Yes");
+        }
+        else {
+            System.out.println("No");
+
+        }
+    }}
